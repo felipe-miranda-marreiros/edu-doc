e9c523e1ac416d2df8c31e2ea357221cc9fbb17b
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-09 17:03:38 -0300

    feat: add husky

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,5 @@
 import { Stack } from 'expo-router'
-import 'react-native-reanimated'
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  )
+  return <Stack />
 }
