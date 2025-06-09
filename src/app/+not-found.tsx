afdada987e1861eaa8660033131df06384abe00b
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-09 16:56:00 -0300

    feat: add prettier

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,11 +1,11 @@
-import { Stack } from "expo-router";
-import { Text, View } from "react-native";
+import { Stack } from 'expo-router'
+import { Text, View } from 'react-native'
 
 export default function NotFoundScreen() {
   return (
     <View>
-      <Stack.Screen options={{ title: "Oops!" }} />
+      <Stack.Screen options={{ title: 'Oops!' }} />
       <Text>404</Text>
     </View>
-  );
+  )
 }
