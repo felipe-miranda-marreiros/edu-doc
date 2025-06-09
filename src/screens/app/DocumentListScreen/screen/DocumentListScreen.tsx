c59ce6fb93a7d9f0271517de1c477ccc7ad067c5
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-09 18:41:28 -0300

    feat: add document list screen

---
+++ src/screens/app/DocumentListScreen/screen/DocumentListScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function DocumentListScreen() {
+  return (
+    <View>
+      <Text>DocumentList</Text>
+    </View>
+  )
+}
--- src/screens/auth/LoginScreen/components/LoginForm.tsx
+++ src/screens/auth/LoginScreen/components/LoginForm.tsx
@@ -1,10 +1,12 @@
 import { Login } from '@/src/entities/login/Login'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'expo-router'
 import { Controller, useForm } from 'react-hook-form'
 import { Button, Text, TextInput, View } from 'react-native'
 import { loginSchema } from './LoginSchema'
 
 export function LoginForm() {
+  const router = useRouter()
   const form = useForm({
     defaultValues: {
       enrollmentId: '5bd95f9d-7c52-4831-ae1e-577d2961b7a7',
@@ -15,6 +17,7 @@ export function LoginForm() {
 
   function onSubmit(data: Login) {
     console.log(data)
+    router.navigate('/protected/document-list')
   }
 
   return (
