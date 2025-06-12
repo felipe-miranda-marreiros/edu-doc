8d89ec33564b160e3ca19f1f06ce578a4598ab35
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-12 09:53:54 -0300

    feat: hide go back button

--- src/shared/components/Screen/Screen.tsx
+++ src/shared/components/Screen/Screen.tsx
@@ -11,17 +11,17 @@ export function Screen({ children, title }: PropsWithChildren<ScreenProps>) {
   const router = useRouter()
 
   function onPress() {
-    if (router.canGoBack()) {
-      router.back()
-    }
+    router.back()
   }
 
   return (
     <View className="flex-1 bg-white p-4">
       <View className="flex-row gap-4 mb-7">
-        <Pressable onPress={onPress}>
-          <AntDesign name="leftcircleo" size={24} color="black" />
-        </Pressable>
+        {router.canGoBack() && (
+          <Pressable onPress={onPress}>
+            <AntDesign name="leftcircleo" size={24} color="black" />
+          </Pressable>
+        )}
         <Text className="text-lg font-semibold">{title}</Text>
       </View>
       {children}
